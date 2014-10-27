f1e20ca2c606f404c98f8fcc964644440730daa4
tianshuai <tianshuaiorc@sina.com>
2014-10-27 14:47:18 +0800

    cofig

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'xiaomajj'
-set :repo_url, 'git@gitlab.com:shuai/xiaomajj.git'
+set :repo_url, 'git@github.com:tianshuai/xiaomajj.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
