134724e5f5e00fc72a53c14992faedd370f27464
tianshuai <tianshuaiorc@sina.com>
2014-10-27 15:17:25 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, 'git@github.com:tianshuai/xiaomajj.git'
 set :deploy_to, '/home/xiaomajijing/xiaomajj2'
 
 # Default value for :scm is :git
-set :scm, :git
+#set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
