d9f283dcc3252d2ad600fdbd9a945975314a366d
TianShuai <tianshuaiorc@sina.com>
2014-11-03 15:53:00 +0800

    pro

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
+set :stage, :production
+set :rails_env, :production
+
 role :app, %w{xiaomajijing@211.157.136.58}
 role :web, %w{xiaomajijing@211.157.136.58}
 role :db,  %w{xiaomajijing@211.157.136.58}
