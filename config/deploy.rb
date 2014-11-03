7a40f8fd40e8e5f36f1bfc28a2cf951fd8226269
TianShuai <tianshuaiorc@sina.com>
2014-11-03 14:54:23 +0800

    modify config/deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, '/home/xiaomajijing/xiaomajj3'
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -65,5 +65,7 @@ namespace :deploy do
       # end
     end
   end
+
+  after :finishing, 'deploy:cleanup'
 
 end
