d5d6d7d39df6b592a652a1ffd5e1ed734d912c69
TianShuai <tianshuaiorc@sina.com>
2014-11-04 09:15:28 +0800

    pma

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
-ls_env = ENV['RAILS_ENV'] || 'development'
+#ls_env = ENV['RAILS_ENV'] || 'development'
 
 threads 4,4
-
+environment 'production'
 bind  "unix:///home/xiaomajijing/xiaomajj3/shared/tmp/sockets/puma.sock"
 pidfile "/home/xiaomajijing/xiaomajj3/current/tmp/pids/puma.pid"
 state_path "/home/xiaomajijing/xiaomajj3/current/tmp/pids/puma.state"
