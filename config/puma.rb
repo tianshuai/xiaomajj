79d01cc47dff88522a30d381cad2ac202963f547
TianShuai <tianshuaiorc@sina.com>
2014-11-04 09:09:56 +0800

    config puma

---
+++ config/puma.rb
@@ -0,0 +1,9 @@
+ls_env = ENV['RAILS_ENV'] || 'development'
+
+threads 4,4
+
+bind  "unix:///home/xiaomajijing/xiaomajj3/shared/tmp/sockets/puma.sock"
+pidfile "/home/xiaomajijing/xiaomajj3/current/tmp/pids/puma.pid"
+state_path "/home/xiaomajijing/xiaomajj3/current/tmp/pids/puma.state"
+
+activate_control_app
