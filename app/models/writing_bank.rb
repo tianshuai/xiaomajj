674f953fabbb117673b2abdd921b839df5bced3c
TianShuai <tianshuaiorc@sina.com>
2014-11-06 14:15:36 +0800

    writing_bank validate

--- app/models/writing_bank.rb
+++ app/models/writing_bank.rb
@@ -4,5 +4,5 @@ class WritingBank < ActiveRecord::Base
 
 
   validates :title,                                length: { maximum: 50, message: '小于50个字符' }
-  validates :content,                                length: { minimum: 2, maximum: 500, message: '长度大于2个字符且小于500个字符' }
+  validates :content,                                length: { minimum: 2, maximum: 2000, message: '长度大于2个字符且小于2000个字符' }
 end
