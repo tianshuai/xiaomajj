4c5b313816f06cd31a91172896d9442f442bcd22
TianShuai <tianshuaiorc@sina.com>
2014-11-07 09:04:35 +0800

    timezone

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ module Xiaomajj
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W(#{config.root}/lib)
+    
+    #时区
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
