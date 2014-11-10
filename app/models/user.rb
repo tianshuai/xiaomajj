1de1a8ee3b70bdaed16c7cb98d8fd30c9a88c789
TianShuai <tianshuaiorc@sina.com>
2014-11-10 15:44:19 +0800

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   ##验证
   validates_presence_of :login,                    message: '不能为空'
-  validates_uniqueness_of :login,				　         message: '用户名已存在'
+  validates_uniqueness_of :login,                   message: '用户名已存在'
   validates :login,                                length: { minimum: 2, maximum: 30, message: '长度大于2个字符且小于30个字符' }
 
   #validates_presence_of :email,                   message: '不能为空'
